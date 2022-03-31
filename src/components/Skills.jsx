5983b8bbf227d04ee68976cf8ae9f47e51521c88
beejgill <beejgill@gmail.com>
2022-03-30 19:08:52 -0600

    fix linting

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import aws from '../assets/aws.png'
-import html from '../assets/html.png'
-import css from '../assets/css.png'
-import javascript from '../assets/javascript.png'
-import react from '../assets/react.png'
+// import aws from '../assets/aws.png'
+// import html from '../assets/html.png'
+// import css from '../assets/css.png'
+// import javascript from '../assets/javascript.png'
+// import react from '../assets/react.png'
 
 function Skills() {
   return (
