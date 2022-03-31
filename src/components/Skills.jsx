0dc96dacaa90a70635bebdf2329ffd7f01db9c74
beejgill <beejgill@gmail.com>
2022-03-30 18:47:24 -0600

    initial commit

---
+++ src/components/Skills.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import aws from '../assets/aws.png'
+import html from '../assets/html.png'
+import css from '../assets/css.png'
+import javascript from '../assets/javascript.png'
+import react from '../assets/react.png'
+
+function Skills() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Skills
